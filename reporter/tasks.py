3dff16c6e7cca0d74b6e6cc16c61a34520268bcd
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-01-22 20:49:40 -0800

    Updated tasks and added forwarding view.

--- reporter/tasks.py
+++ reporter/tasks.py
@@ -11,6 +11,11 @@ from reporter import api_urls
 
 
 @app.task
+def test():
+    return requests.get('https://aswwu.com/server/elections/position').json()
+
+
+@app.task
 def get_watchman_group(group_id, api_key=str()):
     """
     Queries the Watchman Monitoring '/groups/<group_id>' endpoint with a group ID to retrieve information pertaining
@@ -30,15 +35,17 @@ def get_watchman_group(group_id, api_key=str()):
     # return results or error
     if req.status_code != status.HTTP_200_OK:
         raise Exception(f'request returned status code {req.status_code}')
-    return req
+    return req.json()
 
 
 @app.task
-def get_watchman_computers(group_id=None, api_key=str()):
+def get_watchman_computers(page=None, per_page=None, group_id=None, api_key=str()):
     """
     Queries the Watchman Monitoring '/computers' endpoint with a group ID to retrieve a list of all monitored
     computers along with any warnings that these computers currently have.
 
+    :param page: The page number for pagination.
+    :param per_page: The number of computers per page, max of 100.
     :param group_id: The group ID of computers to query.
     :param api_key: An optional API key to use if no WATCHMAN_API_KEY environment variable is set.
     :return: Returns the request response.
@@ -48,12 +55,19 @@ def get_watchman_computers(group_id=None, api_key=str()):
         'api_key': os.getenv('WATCHMAN_API_KEY', api_key),
         'expand[]': 'plugin_results',
     }
+    if page is not None:
+        query_params['page'] = page
+    if per_page is not None:
+        query_params['per_page'] = per_page
     if group_id is not None:
         query_params['group_id'] = group_id
     # make request
     url = api_urls.watchman['computers']
+    print(url)
+    print(query_params)
     req = requests.get(url, query_params)
     # return results or error
     if req.status_code != status.HTTP_200_OK:
         raise Exception(f'request returned status code {req.status_code}')
-    return req
+    return req.json()
+
--- reporter/views.py
+++ reporter/views.py
@@ -1,3 +1,6 @@
+import math
+
+from celery import group
 from rest_framework import views
 from rest_framework import response
 
@@ -6,9 +9,20 @@ from reporter import tasks
 
 class Forward(views.APIView):
     def get(self, request):
-        request = tasks.get_watchman_computers.delay(group_id=request.query_params['group_id'])
-        while not request.ready():
+        # get watchman group information about number of computers
+        watchman_group = tasks.get_watchman_group(request.query_params['group_id'])
+
+        # put the the multiple computer requests in a group
+        results_per_page = 10
+        request_num = math.ceil(watchman_group['visible_computer_count'] / results_per_page) + 1
+        computers_group = group(tasks.get_watchman_computers.s(page=page,
+                                                               per_page=results_per_page,
+                                                               group_id=request.query_params['group_id'])
+                                for page in range(1, request_num))
+        results = computers_group()
+        new_results = list()
+        while not results.ready():
             pass
-        return response.Response(request.result[1])
-
-
+        for r in results.results:
+            new_results += r.result
+        return response.Response(new_results)
