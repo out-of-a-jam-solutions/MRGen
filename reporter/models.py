01e271934c2c7f0ea7f3264144656a3b2f96d13c
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-01-22 20:49:12 -0800

    Added models and migrations.

--- reporter/models.py
+++ reporter/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    watchman_id = models.TextField()
+    repairshopr_id = models.TextField()
+
+
+class WatchmanWarning(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    computer_id = models.TextField()
+    warning_id = models.TextField()
+    date_reported = models.DateField()
+    date_resolved = models.DateField()
