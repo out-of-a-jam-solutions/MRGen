4816477365fdf7ce5015795ca5dd958e5332fd62
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-01-23 20:59:18 -0800

    Added tasks and forwarding workflow to query all of a clients computers in the watchman database.

--- reporter/tasks.py
+++ reporter/tasks.py
@@ -1,9 +1,11 @@
 # sheldon woodward
 # jan 8, 2019
 
+import math
 import os
 import requests
 
+from celery.task import chord
 from rest_framework import status
 
 from MRGen.celery import app
@@ -11,8 +13,12 @@ from reporter import api_urls
 
 
 @app.task
-def test():
-    return requests.get('https://aswwu.com/server/elections/position').json()
+def watchman_update_client(group_id, api_key=str()):
+    # get watchman group information about number of computers
+    results_per_page = 100
+    return (get_watchman_group.s(group_id, api_key) |
+            determine_computer_request_num.s(results_per_page) |
+            queue_watchman_computers_requests.s(results_per_page, group_id, api_key))()
 
 
 @app.task
@@ -36,6 +42,22 @@ def get_watchman_group(group_id, api_key=str()):
     if req.status_code != status.HTTP_200_OK:
         raise Exception(f'request returned status code {req.status_code}')
     return req.json()
+
+
+@app.task
+def determine_computer_request_num(json, per_page):
+    return math.ceil(json['visible_computer_count'] / per_page)
+
+
+@app.task
+def queue_watchman_computers_requests(request_num, per_page, group_id, api_key=str()):
+    # put the the multiple computer requests in a group
+    computers_chord = chord(get_watchman_computers.s(page=page,
+                                                     per_page=per_page,
+                                                     group_id=group_id,
+                                                     api_key=api_key)
+                            for page in range(1, request_num + 1))
+    return computers_chord(combine_watchman_computer_results.subtask())
 
 
 @app.task
@@ -63,11 +85,16 @@ def get_watchman_computers(page=None, per_page=None, group_id=None, api_key=str(
         query_params['group_id'] = group_id
     # make request
     url = api_urls.watchman['computers']
-    print(url)
-    print(query_params)
     req = requests.get(url, query_params)
     # return results or error
     if req.status_code != status.HTTP_200_OK:
         raise Exception(f'request returned status code {req.status_code}')
     return req.json()
 
+
+@app.task
+def combine_watchman_computer_results(*results):
+    new_results = list()
+    for r in results[0]:
+        new_results += r
+    return new_results
--- reporter/views.py
+++ reporter/views.py
@@ -1,28 +1,13 @@
-import math
-
-from celery import group
+from celery.result import AsyncResult, result_from_tuple
 from rest_framework import views
 from rest_framework import response
 
+from MRGen.celery import app
 from reporter import tasks
 
 
 class Forward(views.APIView):
     def get(self, request):
-        # get watchman group information about number of computers
-        watchman_group = tasks.get_watchman_group(request.query_params['group_id'])
-
-        # put the the multiple computer requests in a group
-        results_per_page = 10
-        request_num = math.ceil(watchman_group['visible_computer_count'] / results_per_page) + 1
-        computers_group = group(tasks.get_watchman_computers.s(page=page,
-                                                               per_page=results_per_page,
-                                                               group_id=request.query_params['group_id'])
-                                for page in range(1, request_num))
-        results = computers_group()
-        new_results = list()
-        while not results.ready():
-            pass
-        for r in results.results:
-            new_results += r.result
-        return response.Response(new_results)
+        results = tasks.watchman_update_client(request.query_params['group_id'])
+        res = result_from_tuple(results.get())
+        return response.Response(res.get())
