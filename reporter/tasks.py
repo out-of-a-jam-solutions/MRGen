cfaeb79517f1e11abb5dc4e555ce976e13cf7574
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-02-01 10:24:36 -0800

    Completed all docstrings for current Watchman tasks.

--- reporter/tasks.py
+++ reporter/tasks.py
@@ -70,11 +70,17 @@ def get_watchman_computers(page=None, per_page=None, group_id=None, api_key=str(
 @app.task
 def watchman_update_client(group_id, api_key=str()):
     """
-    Starts the Watchman update tasks for a specific
+    Starts the Watchman update tasks for a specific Watchman group. This is the main task to start the update process
+    for Watchman information. Example:
 
-    :param group_id:
-    :param api_key:
-    :return:
+        results = tasks.watchman_update_client(group_id)
+        res = result_from_tuple(results.get())
+        return response.Response(res.get())
+
+    :param group_id: The group ID to use in requesting a list of computers.
+    :param api_key: An optional API key to use if no WATCHMAN_API_KEY environment variable is set.
+    :return: Returns an AsyncResult object tuple containing another AsyncResult with the results of
+    queue_watchman_computers_requests(). Reference the above example to decode the results.
     """
     # get watchman group information about number of computers
     results_per_page = 100
@@ -85,11 +91,27 @@ def watchman_update_client(group_id, api_key=str()):
 
 @app.task
 def determine_computer_request_num(json, per_page):
+    """
+    Determines the number of requests to make to retrieve all computers.
+
+    :param json: A JSON formatted dictionary containing the results of a Watchman '/groups' request
+    :param per_page: The number of computers that are displayed per page in a Watchman '/computers' request.
+    :return: Returns an integer value with the number of requests that should be made to the Watchman computers API.
+    """
     return math.ceil(json['visible_computer_count'] / per_page)
 
 
 @app.task
 def queue_watchman_computers_requests(request_num, per_page, group_id, api_key=str()):
+    """
+    Creates a Celery chord of requests and concatenates the results into one JSON formatted dictionary.
+
+    :param request_num: The number of requests to make to get all results from the Watchman '/computers' endpoint.
+    :param per_page: The number of computers to include per page. Watchman limits this to 100.
+    :param group_id: The group ID to use in requesting a list of computers.
+    :param api_key: An optional API key to use if no WATCHMAN_API_KEY environment variable is set.
+    :return: Returns a JSON formatted dictionary containing the concatenated results from the multiple requests.
+    """
     # put the the multiple computer requests in a group
     computers_chord = chord(get_watchman_computers.s(page=page,
                                                      per_page=per_page,
@@ -101,6 +123,13 @@ def queue_watchman_computers_requests(request_num, per_page, group_id, api_key=s
 
 @app.task
 def combine_watchman_computer_results(*results):
+    """
+    The callback task used in queue_watchman_computers_requests() to concatenate all the results from Watchman the
+    '/computers' request.
+
+    :param results: List of JSON formatted dictionary results to concatenate.
+    :return: Returns a single JSON formatted dictionary of Watchman '/computers' endpoint results.
+    """
     new_results = list()
     for r in results[0]:
         new_results += r
