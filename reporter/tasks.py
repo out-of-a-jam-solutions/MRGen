338721d7392c6dc4f420b9a0c81d25a6039710ef
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-02-01 10:28:28 -0800

    Reordered tasks.

--- reporter/tasks.py
+++ reporter/tasks.py
@@ -13,6 +13,28 @@ from reporter import api_urls
 
 
 @app.task
+def watchman_update_client(group_id, api_key=str()):
+    """
+    Starts the Watchman update tasks for a specific Watchman group. This is the main task to start the update process
+    for Watchman information. Example:
+
+        results = tasks.watchman_update_client(group_id)
+        res = result_from_tuple(results.get())
+        return response.Response(res.get())
+
+    :param group_id: The group ID to use in requesting a list of computers.
+    :param api_key: An optional API key to use if no WATCHMAN_API_KEY environment variable is set.
+    :return: Returns an AsyncResult object tuple containing another AsyncResult with the results of
+    queue_watchman_computers_requests(). Reference the above example to decode the results.
+    """
+    # get watchman group information about number of computers
+    results_per_page = 100
+    return (get_watchman_group.s(group_id, api_key) |
+            determine_computer_request_num.s(results_per_page) |
+            queue_watchman_computers_requests.s(results_per_page, group_id, api_key))()
+
+
+@app.task
 def get_watchman_group(group_id, api_key=str()):
     """
     Queries the Watchman Monitoring '/groups/<group_id>' endpoint with a group ID to retrieve information pertaining
@@ -33,60 +55,6 @@ def get_watchman_group(group_id, api_key=str()):
     if req.status_code != status.HTTP_200_OK:
         raise Exception(f'request returned status code {req.status_code}')
     return req.json()
-
-
-@app.task
-def get_watchman_computers(page=None, per_page=None, group_id=None, api_key=str()):
-    """
-    Queries the Watchman Monitoring '/computers' endpoint with a group ID to retrieve a list of all monitored
-    computers along with any warnings that these computers currently have.
-
-    :param page: The page number for pagination.
-    :param per_page: The number of computers per page, max of 100.
-    :param group_id: The group ID of computers to query.
-    :param api_key: An optional API key to use if no WATCHMAN_API_KEY environment variable is set.
-    :return: Returns the request response.
-    """
-    # construct query parameters
-    query_params = {
-        'api_key': os.getenv('WATCHMAN_API_KEY', api_key),
-        'expand[]': 'plugin_results',
-    }
-    if page is not None:
-        query_params['page'] = page
-    if per_page is not None:
-        query_params['per_page'] = per_page
-    if group_id is not None:
-        query_params['group_id'] = group_id
-    # make request
-    url = api_urls.watchman['computers']
-    req = requests.get(url, query_params)
-    # return results or error
-    if req.status_code != status.HTTP_200_OK:
-        raise Exception(f'request returned status code {req.status_code}')
-    return req.json()
-
-
-@app.task
-def watchman_update_client(group_id, api_key=str()):
-    """
-    Starts the Watchman update tasks for a specific Watchman group. This is the main task to start the update process
-    for Watchman information. Example:
-
-        results = tasks.watchman_update_client(group_id)
-        res = result_from_tuple(results.get())
-        return response.Response(res.get())
-
-    :param group_id: The group ID to use in requesting a list of computers.
-    :param api_key: An optional API key to use if no WATCHMAN_API_KEY environment variable is set.
-    :return: Returns an AsyncResult object tuple containing another AsyncResult with the results of
-    queue_watchman_computers_requests(). Reference the above example to decode the results.
-    """
-    # get watchman group information about number of computers
-    results_per_page = 100
-    return (get_watchman_group.s(group_id, api_key) |
-            determine_computer_request_num.s(results_per_page) |
-            queue_watchman_computers_requests.s(results_per_page, group_id, api_key))()
 
 
 @app.task
@@ -119,6 +87,38 @@ def queue_watchman_computers_requests(request_num, per_page, group_id, api_key=s
                                                      api_key=api_key)
                             for page in range(1, request_num + 1))
     return computers_chord(combine_watchman_computer_results.subtask())
+
+
+@app.task
+def get_watchman_computers(page=None, per_page=None, group_id=None, api_key=str()):
+    """
+    Queries the Watchman Monitoring '/computers' endpoint with a group ID to retrieve a list of all monitored
+    computers along with any warnings that these computers currently have.
+
+    :param page: The page number for pagination.
+    :param per_page: The number of computers per page, max of 100.
+    :param group_id: The group ID of computers to query.
+    :param api_key: An optional API key to use if no WATCHMAN_API_KEY environment variable is set.
+    :return: Returns the request response.
+    """
+    # construct query parameters
+    query_params = {
+        'api_key': os.getenv('WATCHMAN_API_KEY', api_key),
+        'expand[]': 'plugin_results',
+    }
+    if page is not None:
+        query_params['page'] = page
+    if per_page is not None:
+        query_params['per_page'] = per_page
+    if group_id is not None:
+        query_params['group_id'] = group_id
+    # make request
+    url = api_urls.watchman['computers']
+    req = requests.get(url, query_params)
+    # return results or error
+    if req.status_code != status.HTTP_200_OK:
+        raise Exception(f'request returned status code {req.status_code}')
+    return req.json()
 
 
 @app.task
