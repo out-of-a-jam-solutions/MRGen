0f2976fcf1d9ed230649a53a7534e44083607cfa
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-02-09 18:27:23 -0800

    Refactored method names for watchman tasks. Added warning parsing to the task chain for watchman client updates.

--- reporter/tasks_watchman.py
+++ reporter/tasks_watchman.py
@@ -15,29 +15,29 @@ from reporter import models
 
 
 @shared_task
-def watchman_update_client(group_id, api_key=str()):
+def update_client(group_id, api_key=str()):
     """
     Starts the Watchman update tasks for a specific Watchman group. This is the main task to start the update process
     for Watchman information. Example:
 
-        results = tasks.watchman_update_client(group_id)
+        results = tasks.update_client(group_id)
         res = result_from_tuple(results.get())
         return response.Response(res.get())
 
     :param group_id: The group ID to use in requesting a list of computers.
     :param api_key: An optional API key to use if no WATCHMAN_API_KEY environment variable is set.
     :return: Returns an AsyncResult object tuple containing another AsyncResult with the results of
-    queue_watchman_computers_requests(). Reference the above example to decode the results.
+    queue_computers_requests(). Reference the above example to decode the results.
     """
     # get watchman group information about number of computers
     results_per_page = 100
-    return (get_watchman_group.s(group_id, api_key) |
+    return (get_group.s(group_id, api_key) |
             determine_computer_request_num.s(results_per_page) |
-            queue_watchman_computers_requests.s(results_per_page, group_id, api_key))()
+            queue_computers_requests.s(results_per_page, group_id, api_key))()
 
 
 @shared_task
-def get_watchman_group(group_id, api_key=str()):
+def get_group(group_id, api_key=str()):
     """
     Queries the Watchman Monitoring '/groups/<group_id>' endpoint with a group ID to retrieve information pertaining
     to the group.
@@ -72,7 +72,7 @@ def determine_computer_request_num(json, per_page):
 
 
 @shared_task
-def queue_watchman_computers_requests(request_num, per_page, group_id, api_key=str()):
+def queue_computers_requests(request_num, per_page, group_id, api_key=str()):
     """
     Creates a Celery chord of requests and concatenates the results into one JSON formatted dictionary.
 
@@ -83,16 +83,16 @@ def queue_watchman_computers_requests(request_num, per_page, group_id, api_key=s
     :return: Returns a JSON formatted dictionary containing the concatenated results from the multiple requests.
     """
     # put the the multiple computer requests in a group
-    computers_chord = chord(get_watchman_computers.s(page=page,
-                                                     per_page=per_page,
-                                                     group_id=group_id,
-                                                     api_key=api_key)
+    computers_chord = chord(get_computers.s(page=page,
+                                            per_page=per_page,
+                                            group_id=group_id,
+                                            api_key=api_key)
                             for page in range(1, request_num + 1))
-    return computers_chord(combine_watchman_computer_results.subtask())
+    return computers_chord(combine_computer_results.subtask())
 
 
 @shared_task
-def get_watchman_computers(page=None, per_page=None, group_id=None, api_key=str()):
+def get_computers(page=None, per_page=None, group_id=None, api_key=str()):
     """
     Queries the Watchman Monitoring '/computers' endpoint with a group ID to retrieve a list of all monitored
     computers along with any warnings that these computers currently have.
@@ -124,24 +124,28 @@ def get_watchman_computers(page=None, per_page=None, group_id=None, api_key=str(
 
 
 @shared_task
-def combine_watchman_computer_results(*results):
+def combine_computer_results(*results):
     """
-    The callback task used in queue_watchman_computers_requests() to concatenate all the results from Watchman the
+    The callback task used in queue_computers_requests() to concatenate all the results from Watchman the
     '/computers' request.
 
     :param results: List of JSON formatted dictionary results to concatenate.
     :return: Returns a single JSON formatted dictionary of Watchman '/computers' endpoint results.
     """
+    # build the new results
     new_results = list()
     for r in results[0]:
         new_results += r
+    # update the database with the results
+    parse_warnings.delay(new_results)
+    # return the combined results
     return new_results
 
 
 @shared_task
 def parse_warnings(json):
     """
-    Parses the results of queue_watchman_computers_requests to save new warnings to the database and update existing
+    Parses the results of queue_computers_requests to save new warnings to the database and update existing
     warnings.
 
     :param json: A JSON formatted dictionary containing the concatenated results from the multiple requests.
--- reporter/views.py
+++ reporter/views.py
@@ -8,10 +8,8 @@ from reporter import tasks_watchman
 class Forward(views.APIView):
     def get(self, request):
         # get watchman concatenated results
-        results = tasks_watchman.watchman_update_client(request.query_params['watchman_group_id'])
-        res = result_from_tuple(results.get())
-        results = res.get()
-        # save warnings to database
-        tasks_watchman.parse_warnings(results)
+        results = tasks_watchman.update_client(request.query_params['watchman_group_id'])
+        results = result_from_tuple(results.get())
+        results = results.get()
 
         return response.Response(results)
