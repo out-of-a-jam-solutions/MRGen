f5f52d2570f690fe1f443e88b6628cbd7de13822
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-02-09 19:03:33 -0800

    Installed Celery Beat.

--- MRGen/settings.py
+++ MRGen/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reporter'
+    'django_celery_beat',
+    'reporter',
 ]
 
 MIDDLEWARE = [
