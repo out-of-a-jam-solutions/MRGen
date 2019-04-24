4b252605c93536b65d857e40b0db95a5ab71a582
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-04-24 10:46:40 -0700

    Updated CORS headers on api.

--- MRGen/settings.py
+++ MRGen/settings.py
@@ -30,12 +30,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_celery_results',
     'django_celery_beat',
+    'corsheaders',
     'reporter',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# DJANGO CORS HEADERS
+CORS_ORIGIN_ALLOW_ALL = True
 
 # DJANGO REST FRAMEWORK
 REST_FRAMEWORK = {
