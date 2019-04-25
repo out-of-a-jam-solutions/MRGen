ca9de6fcd5b26c9ac0a55e6a3615ebdd31e69ff6
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-04-25 12:18:47 -0700

    Updated CustomerInfo component.

--- front-end/src/components/CustomerInfo.vue
+++ front-end/src/components/CustomerInfo.vue
@@ -10,7 +10,16 @@ export default {
 </script>
 
 <template>
-  <div>{{ selectedCustomer }}</div>
+  <div v-if="selectedCustomer">
+    <b-card
+      :title="selectedCustomer.name"
+      :sub-title="selectedCustomer.pk"
+    >
+      <b-card-text>
+        Customer information
+      </b-card-text>
+    </b-card>
+  </div>
 </template>
 
 <style scoped></style>
