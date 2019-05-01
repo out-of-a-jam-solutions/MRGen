50ba3b04f3d9f9eb49c3497eda627a4e6e492e05
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-04-30 19:17:53 -0700

    Added site navbar.

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="app" class="container">
-    <div id="nav">
-      <router-link to="/">Customers</router-link> |
-      <router-link to="/reports">Report</router-link>
+  <div id="app">
+    <b-navbar type="dark" variant="primary" class="mb-3">
+      <!-- navbar title -->
+      <b-navbar-brand href="#">MRGen</b-navbar-brand>
+      <!-- navbar content -->
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/">Customers</b-nav-item>
+          <b-nav-item to="/reports">Reports</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <div class="container">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
