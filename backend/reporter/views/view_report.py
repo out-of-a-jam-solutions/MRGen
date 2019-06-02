af5a1a8900a8677aae03abc207ca32a50ec2d0e5
sheldonkwoodward <sheldon.k.woodward@gmail.com>
2019-06-02 10:46:08 -0700

    Added dynamic title metadata to reports and dynamic filenames.

--- backend/reporter/views/view_report.py
+++ backend/reporter/views/view_report.py
@@ -158,10 +158,10 @@ class ReportPDFView(views.APIView):
         except models.Report.DoesNotExist:
             return HttpResponseNotFound()
         # generate the pdf response
-        pdf_response = HttpResponse(content_type="application/pdf")
-        pdf_response['Content-Disposition'] = "inline; filename=report.pdf"
+        pdf_response = HttpResponse(content_type='application/pdf')
+        pdf_response['Content-Disposition'] = 'inline; filename={}'.format(f'report_{report.customer.name}_{report.start_date}_{report.end_date}.pdf')
         # render the PDF
-        html = render_to_string("report.html", {'report': report})
+        html = render_to_string('report.html', {'report': report})
         HTML(string=html).write_pdf(pdf_response)
         # return the PDF response
         return pdf_response
